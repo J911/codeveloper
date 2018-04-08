e553a2569df9bc94de7bdc56b631f31f9882156e
J911 <fb.jaemin@gmail.com>
2018-04-08 21:57:16 +0900

    update Home.vue

---
+++ codeveloper-frontend/src/assets/icons/README.md
@@ -0,0 +1,15 @@
+# Development SVG icon space
+
+This repository contains a collection of vectors in SVG format. At the moment, it is only a simple container of these files. But the idea is to create something similar to [Theodore Vorillas](https://github.com/vorillaz)'s [devicons](http://vorillaz.github.io/devicons/#/main) project.
+
+The big difference between this project and *devicons* is that **my icons have their natural colour**; they are **not in black an white**.
+
+The following sprite contains all the icons of this collection:
+
+![devSVGIconSpace](https://raw.githubusercontent.com/benatespina/devSVGIconSpace/master/sprites/sprites.png)
+
+## Credits
+Created by **benatespina** - [benatespina@gmail.com](mailto:benatespina@gmail.com). Copyright (c) 2014
+
+## Licensing Options
+Released under MIT license. See [LICENSE.md](https://github.com/benatespina/devSVGIconSpace/blob/master/LICENSE.md) file for more information.
--- codeveloper-frontend/src/spa/Home.vue
+++ codeveloper-frontend/src/spa/Home.vue
@@ -1,7 +1,62 @@
 <template>
-  <div id="app">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+  <div id="home">
+    <nav>
+        <span class="brand">codeveloper.io</span>
+        <ul class="menu">
+            <li class="item">
+                introduce
+            </li>
+            <li class="item">
+                developer
+            </li>
+            <li class="item">
+                sign in
+            </li>
+            <li class="item">
+                sign up
+            </li>
+        </ul>
+    </nav>
+    <header>
+        <h1>
+            당신의 팀과 co-develop하세요!
+        </h1>
+        <p><b>codeveloper.io</b>는 실시간 협업코딩 솔루션입니다.</p>
+        <div class="btn-group">
+            <button> <i class="fa fa-code"></i> start code</button>
+            <button class="gray">what the codeveloper</button>
+        </div>
+    </header>
+    <article>
+        <section id="introduce">
+            <i class="fa fa-code"></i>
+            <h1>codeveloper?</h1>
+            <p>codeveloper는 여러 그룹이 동시에 소스코드를 개발 및 수정 할 수 있도록 개발된 웹 IDE입니다.</p>
+            <p>실시간 코드 동기화와 컴파일을 codeveloper에서 시작해보세요!</p>
+        </section>
+        <hr>
+        <section id="language">
+            <h1>현재 지원언어</h1>
+            <ul class="languages">
+                <li class="item">
+                    <span class="icon">C</span> 
+                    <p>C language</p>
+                </li>
+                <li class="item">
+                    <span class="icon"><i class="fab fa-node-js"></i></span>
+                    <p>Node Js</p>
+                </li>
+                <li class="item">
+                    <span class="icon"><i class="fab fa-python"></i></span>
+                    <p>Python</p>
+                </li>
+            </ul>
+            <p class="sub-contents">원하는 언어가 없나요?</p>
+        </section>
+    </article>
+    <footer>
+        <a class="github" href="https://github.com/J911" target="_blank"><i class="fab fa-github"></i> J911</a>
+    </footer>
   </div>
 </template>
 
@@ -10,8 +65,124 @@ export default {
   name: 'Home',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Home Page'
     }
   }
 }
 </script>
+
+<style scoped>
+    hr {
+        width: 30px;
+        margin: 20px auto;
+        border: 2px solid rgb(248, 107, 107); 
+    }
+    nav {
+        height: 30px;
+        line-height: 30px;
+        padding: 10px 20px;
+    }
+    nav span.brand {
+        font-family: 'Oswald', sans-serif;
+        float: left;
+    }
+    nav ul.menu {
+        float: right;
+    }
+    nav ul.menu li.item {
+        display: inline-block;
+        padding: 0 15px;
+        cursor: pointer;
+    }
+    header {
+        background-image: url('../assets/header_bg.png');
+        background-position: center;
+        text-align: center;
+        padding: 100px 20px;
+        color: #fff;
+        height: 400px;
+        box-sizing: border-box;
+    }
+    header h1 {
+        line-height: 80px;
+        font-size: 3rem;
+        opacity: 0.9;
+    }
+    header p {
+        opacity: 0.8;
+    }
+    header .btn-group {
+        margin: 50px 0;
+    }
+    header button {
+        width: 200px;
+        height: 40px;
+        font-size: 1rem;
+        border: none;
+        margin: 0 20px;
+        font-weight: 600;
+        background-color: rgb(220, 101, 101);
+        border-radius: 3px;
+        color: #fff;
+        box-shadow: 0 0 10px rgb(106, 106, 106);
+    }
+    header button:active {
+        background-color: rgb(218, 84, 84);
+    }
+    header button:focus {
+        outline: none;
+    }
+    .gray {
+        background-color: gray;
+    }
+    .gray:active {
+        background-color: rgb(129, 118, 118);
+    }
+    article {
+        padding: 20px;
+    }
+    article section {
+        padding: 10px;
+    }
+    article section#introduce {
+        text-align: center;
+    }
+    article section#language {
+        text-align: center;
+    }
+    article section#introduce > i {
+        font-size: 3rem;
+    }
+    article section#introduce h1 {
+        line-height: 50px;
+    }
+    article section#language h1 {
+        line-height: 50px;
+    }
+    article section#language ul.languages {
+        padding: 20px 0;
+        list-style: none;
+    }
+    article section#language ul.languages li.item {
+        display: inline-block;
+        padding: 0 20px;
+    }
+    span.icon {
+        font-weight: 800;
+        font-size: 2rem;
+    }
+    article section#language .sub-contents {
+        font-size: 0.8rem;
+        color: gray;
+        cursor: pointer;
+    }
+    footer {
+        text-align: center;
+        font-weight: 800;
+        padding: 20px 0;
+    }
+    footer a.github {
+        color: black;
+        text-decoration: none;
+    }
+</style>
