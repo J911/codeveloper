e98a998ca7735ee2fb527bc143e97f3180abfbdc
J911 <fb.jaemin@gmail.com>
2018-04-09 17:18:20 +0900

    update route auth.js

--- codeveloper-backend/src/routes/auth.js
+++ codeveloper-backend/src/routes/auth.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const passport = require('../passport');
 
-router.get('/auth/github', passport.authenticate('github')); 
-router.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login' }), (req, res) => {
+router.get('/github', passport.authenticate('github')); 
+router.get('/github/callback', passport.authenticate('github', { failureRedirect: '/login' }), (req, res) => {
     // Successful authentication, redirect home. 
     res.redirect('/');
 });
