02efb36d35482502d0c2658be77df6a5e349dee3
J911 <fb.jaemin@gmail.com>
2018-04-09 22:05:28 +0900

    update div css

--- codeveloper-frontend/src/spa/components/Nav.vue
+++ codeveloper-frontend/src/spa/components/Nav.vue
@@ -20,31 +20,4 @@
   
 </template>
 
-<style scoped>
-nav {
-    height: 30px;
-    line-height: 30px;
-    padding: 10px 20px;
-}
-nav a.brand {
-    color: #000;
-    text-decoration: none;
-    font-family: 'Oswald', sans-serif;
-    float: left;
-}
-nav ul.menu {
-    float: right;
-    margin: 0;
-}
-nav ul.menu li.item {
-    display: inline-block;
-    padding: 0 15px;
-    cursor: pointer;
-    font-weight: 400;
-    color:rgb(52, 52, 52);
-}
-nav ul.menu li.item:hover {
-    color: rgb(232, 119, 119);
-    transition: .2s all;
-}
-</style>
+<style src="./Nav.css" scoped></style>
