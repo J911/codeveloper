c2d6ba818c9744d547984fd520dbb6df642e4940
J911 <fb.jaemin@gmail.com>
2018-04-09 22:18:35 +0900

    update add IDE.vue

---
+++ codeveloper-frontend/src/spa/IDE.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="IDE">
+    <img :src="user.user_avatar" :alt="user.user_name">
+    <span>hello {{ user.user_name }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IDE',
+  data () {
+    return {
+      user: {
+        user_name: "Jay",
+        user_avatar: "https://avatars0.githubusercontent.com/u/34212724?v=4"
+      }
+    }
+  }
+}
+</script>
+
