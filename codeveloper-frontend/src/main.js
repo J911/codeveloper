0fe46ee534bac8af0a4c63731ecbfae844fcd0fe
J911 <fb.jaemin@gmail.com>
2018-04-09 22:35:40 +0900

    update setup axios

--- codeveloper-frontend/src/main.js
+++ codeveloper-frontend/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import router from './config/router'
+import axios from 'axios'
 
 import App from './App.vue'
 
+Vue.prototype.$http = axios
 Vue.use(VueRouter)
 
 new Vue({
