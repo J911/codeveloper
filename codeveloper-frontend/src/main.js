0c573aee4e591723808e64a817b23a614e3a6e2d
J911 <fb.jaemin@gmail.com>
2018-04-10 17:49:09 +0900

    update change socket.io

--- codeveloper-frontend/src/main.js
+++ codeveloper-frontend/src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import router from './config/router'
 import axios from 'axios'
-import VueSocketio from 'vue-socket.io';
+import VueSocketio from 'vue-socket.io-extended'
+import io from 'socket.io-client'
 import VueCodemirror from 'vue-codemirror'
 import 'codemirror/lib/codemirror.css'
 
@@ -11,7 +12,7 @@ import App from './App.vue'
 Vue.prototype.$http = axios
 
 Vue.use(VueRouter)
-Vue.use(VueSocketio,'http://127.0.0.1:3000');
+Vue.use(VueSocketio, io('http://127.0.0.1:3000'))
 Vue.use(VueCodemirror,  { 
   options: { theme: 'base16-dark' },
   events: ['scroll']
