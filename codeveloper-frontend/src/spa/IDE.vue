fa8581a572a4cc5935ad4fa4deb229f324c6fb37
J911 <fb.jaemin@gmail.com>
2018-04-10 22:29:29 +0900

    update div css

--- codeveloper-frontend/src/spa/IDE.vue
+++ codeveloper-frontend/src/spa/IDE.vue
@@ -28,6 +28,10 @@
       <ul class="files">
         <li class="header">
           My Files
+          <span class="new-items">
+            <i class="fas fa-file"></i>
+            <i class="fas fa-folder-open"></i>
+          </span>
         </li>
         <li class="item">
 
@@ -115,116 +119,4 @@ export default {
 }
 </script>
 
-<style scope>
-  a {
-    text-decoration: none;
-    color: rgb(235, 235, 235);
-  }
-  nav {
-    position: fixed;
-    top:0;
-    left: 0;
-    right: 0;
-    height: 40px;
-    background-color: #3c3c3c;
-    box-shadow: 0 0 10px #060505;
-    z-index: 999;
-    padding: 0 20px;
-  }
-  nav a.brand {
-    font-family: 'Oswald', sans-serif;
-    float: left;
-    line-height: 40px;
-  }
-  nav ul.menu{
-    float: right;
-    list-style: none;
-    margin: 0;
-  }
-  nav ul.menu li.item {
-    display: inline-block;
-    color: rgb(235, 235, 235);
-    margin: 0 10px;
-    cursor: pointer;
-  }
-  nav .profile img {
-    height: 30px;
-    border-radius: 6px;
-    margin: 5px 5px 5px 20px;
-  }
-  nav span {
-    vertical-align: 70%;
-  }
-  .explorer {
-    position: absolute;
-    top: 40px;
-    left: 0;
-    bottom: 0;
-    display: inline-block;
-    width: 20%;
-    background-color: #252526;
-    z-index: 999;
-    color: rgb(199, 199, 199);
-
-  }
-  .explorer > .header {
-    padding: 20px;
-
-  }
-  .explorer ul.files {
-    margin: 0;
-    padding: 0;
-    list-style: none;
-  }
-  .explorer ul.files li {
-    height: 30px;
-    line-height: 30px;
-  }
-  .explorer ul.files li.header {
-    background-color: #3c3c3c;
-    padding-left: 20px;
-    font-weight: 800;
-
-  }
-  .editor {
-    position: absolute;
-    top: 40px;
-    right: 0;
-    bottom: 30%;
-    display: inline-block;
-    width: 80%;
-    background-color: #151515;
-  }
-  .console {
-    position: absolute;
-    top: 70%;
-    right: 0;
-    bottom: 0;
-    display: inline-block;
-    width: 80%;
-    background-color: #151515;
-    border-top: 1px solid rgb(57, 57, 57);
-  }
-  .console .header ul.menu{
-    list-style: none;
-    color: rgb(171, 171, 171);
-    margin: 0;
-    padding: 20px;
-  }
-  .console .header ul.menu li.item{
-    display: inline-block;
-    padding-bottom: 10px;
-    margin: 0 10px;
-    cursor: pointer;
-  }
-  .CodeMirror {
-    height: auto;
-  }
-  .CodeMirror-scroll {
-    height: 62vh;
-  }
-  .console .header ul.menu li.item.active {
-    color: rgb(206, 206, 206);
-    border-bottom: 2px solid rgb(181, 181, 181);
-  }
-</style>
+<style src="./IDE.css" scope></style>
