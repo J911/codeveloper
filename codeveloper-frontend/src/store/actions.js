b9a88bc38d572ef6a8144668030831e9ddb1f3f1
J911 <fb.jaemin@gmail.com>
2018-04-11 12:00:23 +0900

    update remove baseURL

--- codeveloper-frontend/src/store/actions.js
+++ codeveloper-frontend/src/store/actions.js
@@ -2,16 +2,14 @@ import axios from 'axios'
 
 const actions = {
     getUser: function (context) {
-        const baseURI = '/user'
-        axios.get(`${baseURI}`)
+        axios.get(`/user`)
         .then((result) => {
           //statusCode 비교가 필요함.
           return context.commit('updateUser',result.data.user)
         })
     },
     getFileList: function (context, payload) {
-        const baseURI = '/file'
-        axios.get(`${baseURI}`)
+        axios.get(`/file`)
         .then((result) => {
             //statusCode 비교가 필요함.
             this.code = result.data.code
@@ -19,18 +17,15 @@ const actions = {
         })
     },
     getFile(context, payload) {
-        const baseURI = '/file'
-        axios.get(`${baseURI}/${payload}`)
+        axios.get(`/file/${payload}`)
         .then((result) => {
           //statusCode 비교가 필요함.
           return context.commit('updateFile', result.data.code)
         })
     },
     updateFile(context, payload) {
-        const baseURI = '/file'
-        axios.post(`${baseURI}/${payload.idx}`, {code: payload.code})
+        axios.post(`/file/${payload.idx}`, {code: payload.code})
         return context.commit('updateFile', payload.code)
-
     }
 }
 
