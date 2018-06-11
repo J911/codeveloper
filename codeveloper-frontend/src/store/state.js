0ae44c17f9eb7b9d6365a07f085a2eea7e543de3
Jay <fb.jaemin@gmail.com>
2018-06-11 13:35:31 +0900

    update airbnb js style guide

--- codeveloper-frontend/src/store/state.js
+++ codeveloper-frontend/src/store/state.js
@@ -1,43 +1,43 @@
 export default {
-    user: {
-        user_id: null,
-        user_avatar: null
-      },
-      messageBox: {
-        show: false,
-        title: 'Message',
-        contents: '',
-      },
-      profileBox: {
-        show: false,
-        contributorIdx: null
-      },
-      registBox: {
-        show: false
-      },
-      env: {
-        loading: true,
-        dimmer: false,
-        consoleMenu: 'terminal',
-      },
-      ide: {
-        terminalLogs: [],
-        code: '',
-        codeState: 'basic',
-        options: {
-          tabSize: 4,
-          mode: 'text/javascript',
-          theme: 'base16-dark',
-          lineNumbers: true,
-          line: true
-        },
-        file: {
-          files: [],
-          contributorFiles: [],
-          currentIdx: null,
-          currentMaster: null
-        },
-      },
-      chat: [],
-      contributors: [],
+  chat: [],
+  contributors: [],
+  registBox: {
+    show: false
+  },
+  user: {
+      user_id: null,
+      user_avatar: null
+  },
+  profileBox: {
+    show: false,
+    contributorIdx: null
+  },
+  messageBox: {
+    show: false,
+    title: 'Message',
+    contents: '',
+  },
+  env: {
+    loading: true,
+    dimmer: false,
+    consoleMenu: 'terminal',
+  },
+  ide: {
+    terminalLogs: [],
+    code: '',
+    codeState: 'basic',
+    options: {
+      tabSize: 4,
+      mode: 'text/javascript',
+      theme: 'base16-dark',
+      lineNumbers: true,
+      line: true
+    },
+    file: {
+      files: [],
+      contributorFiles: [],
+      currentIdx: null,
+      currentMaster: null
+    },
+  },
 }
